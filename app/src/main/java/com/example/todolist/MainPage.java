8ee8a0bd0586e114a29fc6f8821c3d65821bd112
tls1454 <tls1454@naver.com>
2019-11-21 16:13:53 +0900

    Add plus connenct
    add

--- app/src/main/java/com/example/todolist/MainPage.java
+++ app/src/main/java/com/example/todolist/MainPage.java
@@ -2,7 +2,10 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainPage extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
+    }
+
+    public void onPlus(View v) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
